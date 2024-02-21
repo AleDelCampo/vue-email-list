d3369cfaa5a5b3b47f47fe8aef20b62ccae4acf4
AleHashirama <alehashirama@gmail.com>
2024-02-21 16:21:24 +0100

    Milestone completata. inizio Bonus e stilizzazione.

---
+++ script.js
@@ -0,0 +1,24 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+
+      emails: [],
+
+    };
+  },
+
+  mounted() {
+
+    for (let i = 0; i < 10; i++) { 
+
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+
+        .then((result) => {
+
+          this.emails.push(result.data.response);
+        })
+    }
+  },
+}).mount('#app');
