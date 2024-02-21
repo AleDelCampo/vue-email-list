3b3f542c3c8b9da176116ab100e49a35b8ee16e0
AleHashirama <alehashirama@gmail.com>
2024-02-21 17:31:11 +0100

    bonus completi

--- script.js
+++ script.js
@@ -3,22 +3,44 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-
       emails: [],
-
+      loading: false
     };
   },
 
-  mounted() {
+  async mounted() {
 
-    for (let i = 0; i < 10; i++) { 
-
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-
-        .then((result) => {
-
-          this.emails.push(result.data.response);
-        })
-    }
+    await this.freshEmails();
   },
-}).mount('#app');
+
+  methods: {
+
+    async freshEmails() {
+
+      try {
+
+        this.loading = true;
+
+        const getEmail = [];
+
+        for (let i = 0; i < 10; i++) {
+
+          getEmail.push(axios.get("https://flynn.boolean.careers/exercises/api/random/mail"));
+        }
+
+        const showEmail = await Promise.all(getEmail);
+
+        this.emails = showEmail.map(response => response.data.response);
+
+      } finally {
+
+        this.loading = false;
+      }
+    },
+
+    async refreshEmails() {
+
+      await this.freshEmails();
+    }
+  }
+}).mount("#app");
